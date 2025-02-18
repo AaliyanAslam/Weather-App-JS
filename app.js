cfab11c2e46c3630b572ef98e487259c44633947
Adil Aslam <aaliyanaslam22@gmail.com>
2025-02-18 14:56:05 +0500

    data rendar and ul design done

--- app.js
+++ app.js
@@ -12,20 +12,33 @@ form.addEventListener('submit' , (event) => {
    axios(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${cityInput.value}&aqi=no`)
 .then((response) => {
    console.log(response.data);
-   response.data.map((data) => {
-      result.innerHTML = `
-    <h1> ${data.location.name} </h1>
-
-
-      `;
+   // var list = document.createElement('ul');
+   result.innerHTML += `
+   <span class= "border-div"> 
+   <span class = "weather-details text-light"> 
+   <span class = "location-name">${response.data.location.name}, ${response.data.location.country}</span>
+   <div class = "d-flex justify-content-center align-items-center main-d"> 
+   <img class ="icon" src="${response.data.current.condition.icon}" alt="">
  
-      
-   })
+      <p>Temperature: ${response.data.current.temp_c}°C</p>
+
+   <p>Wind Speed: ${response.data.current.wind_kph} km/h</p>
+   
+   </div>
+
+   </span>
+   
+   </span>
+   
+   `;
+   cityInput.value = "";
 
 })
-  .catch((error) => {
-   console.error(error);
-});
+//   .catch((error) => {
+//    console.error(error);
+//    alert("no city found")
+//    cityInput.value = "";
+// });
 
 })
 
