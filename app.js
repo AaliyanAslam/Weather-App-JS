d188a96db40412d9a653cd95704529b26e5632ed
Adil Aslam <aaliyanaslam22@gmail.com>
2025-02-18 16:28:53 +0500

    changing s

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ form.addEventListener('submit' , (event) => {
 // console.log(cityInput.input);
  
  
-   axios(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${cityInput.value}&aqi=no`)
+   axios(`https://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${cityInput.value}&aqi=no`)
 .then((response) => {
    console.log(response.data);
    // var list = document.createElement('ul');
