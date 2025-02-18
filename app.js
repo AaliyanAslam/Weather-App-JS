ce56f3a7c4f14e834913ffa40f4cbc0786fa39a9
Adil Aslam <aaliyanaslam22@gmail.com>
2025-02-18 16:24:34 +0500

    moved to axios

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ form.addEventListener('submit' , (event) => {
 // console.log(cityInput.input);
  
  
-   fetch(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${cityInput.value}&aqi=no`)
-   .then((response => response.json()))
+   axios(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${cityInput.value}&aqi=no`)
 .then((response) => {
    console.log(response.data);
    // var list = document.createElement('ul');
